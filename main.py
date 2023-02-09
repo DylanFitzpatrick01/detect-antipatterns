3caf152b8853ddc7173250fa394b331f4caa9aa2
penrosed <24257802+penrosed@users.noreply.github.com>
2023-02-09 17:14:12 +0000

    Added support for command-line args
    Rather than hard-coding the file, you can now specify the file you wish to view with command-line arguments.
    
    If no file is specified, the program will ask.
    
    For VSCode, added a Launch.json where the command line arguments can be specified.

--- main.py
+++ main.py
@@ -1,96 +1,19 @@
 import clang.cindex
+import sys
 
-s = '''
-#include <iostream>
-#include <memory>
-#include <mutex>
-
-class IMyCallback                                                   // Callback interface
-{
-public:
-   virtual ~IMyCallback() = 0;
-
-   virtual void onSomethingHappend(int value) = 0;
-   virtual void onErrorHappend() = 0;
-};
-
-class MyClass                                                       // Class that needs to be made threadsafe.
-{
-public:
-    MyClass(std::shared_ptr<IMyCallback> callback)
-        :  mCallback(callback)
-    {}
-
-    void doSomething()
-    {
-        {
-            std::lock_guard<std::mutex> lock(mDataMutex);           // CriticalSection starts
-
-            calculate();
-
-            mCallback->onSomethingHappend(mInternalState);          // Uh oh! We're calling outside of a locked scope
-        }                                                           // CriticalSection ends
-    }
-
-    int readState()
-    {
-        std::lock_guard<std::mutex> lock(mDataMutex);               // Accessing the same lock as in `doSomething()`
-
-        return mInternalState;
-    }
-
-private:
-
-    void calculate()
-    {
-        mInternalState = mInternalState + 1;                        // Modifying some internal state that needs to be synchronised.
-    }
-
-   std::shared_ptr<IMyCallback> mCallback;
-   std::mutex mDataMutex;
-   int mInternalState = 0;
-};
-
-
-class ReEntrantCallbackImpl : public IMyCallback                     // Implementation of callback interface 
-{
-public:
-    ReEntrantCallbackImpl()
-    {
-    }
-
-    void init(std::shared_ptr<MyClass> myClass)
-    {
-        mMyClass = myClass;
-    }
-
-    void onSomethingHappend(int value) override
-    {
-        std::cout << "Current value is: " << mMyClass->readState(); // Calling `mMyClass->readState()` will access the lock. 
-    }
-
-    void onErrorHappend() override
-    {
-
-    }
- private:
-  std::shared_ptr<MyClass> mMyClass;
-};
-
-int main()
-{
-    auto callback = std::make_shared<ReEntrantCallbackImpl>();
-
-    auto myClass = std::make_shared<MyClass>(callback);
-    callback->init(myClass);
-
-    myClass->doSomething();                                         // Deadlock or crash
-}
-'''
+s = ""
+if (len(sys.argv) > 1):
+    s = open(sys.argv[1], "r")
+else:
+    try:
+        s = open(input("Enter the name of the file you'd like to analyse\n > "), "r")
+    except FileNotFoundError:
+        print("FILE DOES NOT EXIST!\n")
+        exit()
 
 idx = clang.cindex.Index.create()
 tu = idx.parse('tmp.cpp', args=['-std=c++11'],  
-                unsaved_files=[('tmp.cpp', s)],  options=0)
+                unsaved_files=[('tmp.cpp', s.read())],  options=0)
 f = open("AST.txt", "w")                
 for t in tu.get_tokens(extent=tu.cursor.extent):
     f.write(t.spelling + " ")
