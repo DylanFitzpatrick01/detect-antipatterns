52bad19ccfd5a7ecb75cd98d05870c7bd4f7307f
penrosed <24257802+penrosed@users.noreply.github.com>
2023-02-09 17:34:42 +0000

    Restructured main, added helpful comments
    Restructured the code, added comments that explain what the code does.
    
    After generating AST.txt, rather than printing the numerical value of each token, it counts the number of tokens and prints them to the screen.

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import clang.cindex
 import sys
 
+# Attempt to get a filename from the command line args.
+# If that fails, ask the user.
+# If both fail, give up.
 s = ""
-if (len(sys.argv) > 1):
-    s = open(sys.argv[1], "r")
-else:
-    try:
+try:
+    if (len(sys.argv) > 1):
+        s = open(sys.argv[1], "r")
+    else:
         s = open(input("Enter the name of the file you'd like to analyse\n > "), "r")
-    except FileNotFoundError:
-        print("FILE DOES NOT EXIST!\n")
-        exit()
+except FileNotFoundError:
+    print("FILE DOES NOT EXIST!\n")
+    exit()
 
+# Gets clang to start parsing the file, and generate an Abstract Syntax Tree.
 idx = clang.cindex.Index.create()
 tu = idx.parse('tmp.cpp', args=['-std=c++11'],  
                 unsaved_files=[('tmp.cpp', s.read())],  options=0)
-f = open("AST.txt", "w")                
+
+# Remember the amount of tokens we come across. We'll print it later.
+num_tokens = 0
+
+# Generate a textual representation of the AST, in AST.txt.
+f = open("AST.txt", "w")
 for t in tu.get_tokens(extent=tu.cursor.extent):
     f.write(t.spelling + " ")
     f.write("<- type = " + t.kind.name + "\n")
-    print(t.kind.value)
+    # print(t.kind.value) # Prints the numerical value of each token
+    num_tokens += 1
+
+# Print the number of tokens.
+print("\nNumber of tokens in given file:", num_tokens, "\n")
