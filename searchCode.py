3882ba238cd4789feeae1ddce7bce4c8e3f1f72d
Dylan Fitzpatrick <dylanfitzpatrick33@gmail.com>
2023-02-16 14:39:40 +0000

    placed test files into folder

--- searchCode.py
+++ searchCode.py
@@ -1,6 +1,6 @@
 import re
-file_path1 = r"deadlock code"
-file_path2 = r"test1"
+file_path1 = r"test files/deadlock code"
+file_path2 = r"test files/test1"
 
 ordered_list = []
 
@@ -36,12 +36,3 @@ with open(file_path1, "r") as f:
         if ordered_list[i] == ordered_list[i-1]:
             if ordered_list[i] == searchFor1:
                 print("error2" , searchFor1," is repeated twice")
-            
-            
-                
-                
-
-        
-
-
-   
