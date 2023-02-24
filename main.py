7fa914569be56f4e9d9bfbb968d9f02dc7cb85ef
elliot-lyons <elliot243lyons@gmail.com>
2023-02-24 10:43:47 +0000

    Final tidy up, ready to present

--- main.py
+++ main.py
@@ -44,12 +44,13 @@ def main():
             print("Press 1 for public mutex members check")
             print("Press 2 for immutable objects check")
             print("Press 3 for manual lock/unlock check")
-            print("Press 4 for mutex order and out of scope mutex call checks")
+            print("Press 4 for mutex order check")
+            print("Press 5 for out of scope mutex call checks")
 
             while okInput == False:
-                choose = input("Please enter a number between 1 and 4\n")
+                choose = input("Please enter a number between 1 and 5:\n")
 
-                if choose == "1" or choose == "2" or choose == "3" or choose == "4": 
+                if choose == "1" or choose == "2" or choose == "3" or choose == "4" or choose == "5": 
                     choice = choose
                     okInput = True
                 else:
@@ -82,8 +83,11 @@ def main():
                 print("Checking for missing manual locks/unlocks...\n") 
                 missing_unlock(tu)
             elif choice == "4":
-                print("Checking the order of mutexes and whether they are called out of scope...\n")
-                tests(s, False, True)
+                print("Checking the order of mutexes\n")
+                tests(s, True, True)
+            elif choice == "5":
+                print("Checking whether mutexes are called out of scope\n")
+                tests(s, False, True)    
             else:
                 print("Shouldn't get here.")     
 
