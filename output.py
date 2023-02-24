f5c144117f6647cf1fd0c5cc15d62448e244dee9
Daniel Penrose <penrosed@tcd.ie>
2023-02-24 18:11:04 +0000

    print_error() accepts TextIOWrappers
    - The function now accepts either a string containing the file's path, or a TextIOWrapper object of the file itself.
    - Updated the comments
    - print_error() now has a colours dict entry for "greyed out".

--- output.py
+++ output.py
@@ -1,51 +1,64 @@
-import os, re
+import os, re, typing
 
-# given a filename, a (line, column) tuple, and an error string,
-# show the error message pointing at our location.
-# Multi-line error messages are supported.
-def print_error(filename: str, location: tuple, error_msg: str, severity: str="error"):
-
-    # Gets the C++ file, removes comments, then saves it  as an array, with one entry per line.
-    lines = remove_comments("".join(open(filename).readlines()[0:])).splitlines()
+# Arguments:
+# file:     Either the path of the file as a string - "filename", or a TextIOWrapper of the file - open("filename")
+# location: A line-column tuple - (line, column)
+# message:  A string (possibly multi-line), containing the error message - "message"
+# severity: OPTIONAL, "error" by default. A string, containing the severity of the message - "warning"
+# 
+# prints the given error message in a fancy way.
+# 
+def print_error(file: str | typing.TextIO, location: tuple, message: str, severity: str="error"):
 
     # Our colours! Light red for error, yellow for warning.
-    highlight = { "error" : "light red",  "warning" : "yellow" }
+    colours = { "error"      : "light red",
+                "warning"    : "yellow",
+                "greyed out" : "dark grey" }
+
+    if type(file) == str:   # If the 'file' variable is a string, convert it to a TextIOWrapper.
+        file = open(file)
+
+    # Gets the C++ file, removes comments, then saves it  as an array, with one entry per line.
+    lines = remove_comments("".join(file.readlines()[0:])).splitlines()
     
-    # If the location exists...
+    # If the location exists in the file...
     if (location[0] <= len(lines) and location[1] <= len(lines[location[0]-1])
         and location[1] > 0 and location[0] > 0):
 
-        # Tell the user where the error/warning was found...
-        colour("dark grey")
-        print(("-"*5)+"In",filename,str(location)+("-"*5))
+        # Tell the user the file name and location.
+        term_colour(colours["greyed out"])
+        print(("-"*5)+"In",file.name,str(location)+("-"*5))
 
+        # for: the line before the error line (if it exists), the error line,
+        # and the line after the error line (if it exists).
         for index in range(max(location[0]-2,0),min(location[0]+1,len(lines))):
 
             if (index != location[0]-1): # Print non-error lines in dark grey.
-                colour("dark grey")
+                term_colour(colours["greyed out"])
                 print((str(index+1)+": "+lines[index]))
-                colour("native")
+                term_colour("native")
             else:
                 # Print the line our location points to.
                 print(str(index+1) + ": " + lines[index][0:location[1]-1], end='')
-                colour(text = "black", background=highlight[severity])
+                term_colour(text = "black", background=colours[severity])
                 print(lines[index][location[1]-1:len(lines[index])], end='\033[m')
+
                 # Print our error message, offset to align with the error.
                 print("\n" + (" "*(location[1]-1+len(str(index+1))+2)), end='')
-                colour(highlight[severity])
-                print("^" + error_msg.replace("\n", "\n"+" "*(location[1]+len(str(index+1))+2)))
-                colour("native")
+                term_colour(colours[severity])
+                print("^" + message.replace("\n", "\n"+" "*(location[1]+len(str(index+1))+2)))
+                term_colour("native")
     
     else: # If we call for a location that isn't in the file, complain!
-        colour(background="light red")
-        print("print_error(): location",location,"does not exist in",filename)
-        colour("native")
+        term_colour(background="light red")
+        print("print_error(): location",location,"does not exist in",file.name)
+        term_colour("native")
 
 
 # Change the colour of the terminal, using ANSI colour codes.
 # The first input string is the text colour, The second is the background.
 # A list of supported colours can be found below. (native = default colours)
-def colour(text: str="native", background: str="native"):
+def term_colour(text: str="native", background: str="native"):
 
     os.system("") # Gets ANSI colour codes working on windows.
 
@@ -104,3 +117,6 @@ def colour(text: str="native", background: str="native"):
 # Removes C-Style comments in a multi-line string.
 def remove_comments(text):
     return re.sub(re.compile(r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"', re.DOTALL | re.MULTILINE), "", text)
+
+if __name__ == "__main__":
+    print_error(open("test.cpp"), (7,5), "YO!")
