7b94f9582b597bc32af52bbea1f41b304f94ffc1
Leon Byrne <byrnel58@tcd.ie>
2023-03-17 22:34:53 +0000

    Removed observer.py

--- src/test.py
+++ src/test.py
@@ -1,7 +1,6 @@
 import main, os, importlib.util, clang.cindex
 from typing import List
 from alerts import Alert
-from observer import *
 from formalCheckInterface import FormalCheckInterface
 
 # --------UNIT TESTS------- #
@@ -125,52 +124,6 @@ def test_public_mutex_members():
     # Check a file without them.
     alerts: List[Alert] = run_check_on_file("../Checks/manualLockUnlock.py", "../cpp_tests/immutable.cpp")
     assert len(alerts) == 0
-
-# Unit test for observers which are used in some antipatterns
-def test_observers():
-    eventSrc = EventSource()
-    eventSrc2 = EventSource()
-    mutex_observer = tagObserver("std::mutex")
-    lock_guard_observer = tagObserver("std::lock_guard<std::mutex>")
-    declared_variable_observer = cursorKindObserver(clang.cindex.CursorKind.FIELD_DECL)
-    class_observer = cursorKindObserver(clang.cindex.CursorKind.CLASS_DECL)
-    function_observer = cursorKindObserver(clang.cindex.CursorKind.CXX_METHOD)
-    record_observer = cursorTypeKindObserver(clang.cindex.TypeKind.RECORD)
-    
-    assert(eventSrc.observers) == []
-    eventSrc.addMultipleObservers([mutex_observer, lock_guard_observer, declared_variable_observer, class_observer, record_observer])
-    eventSrc.addObserver(function_observer)
-    assert(eventSrc.observers) == [mutex_observer, lock_guard_observer, declared_variable_observer, class_observer, record_observer, function_observer]
-
-    eventSrc2.addMultipleObservers([mutex_observer, lock_guard_observer, class_observer, function_observer])
-    eventSrc2.removeMultipleObservers([mutex_observer, lock_guard_observer, class_observer])
-    assert(eventSrc2.observers) == [function_observer]
-    
-    
-    searchNodes(eventSrc=eventSrc, file_path="../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp")
-    
-    correct_output = (
-        "Detected a 'std::mutex', Name: 'mDataAccess' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 17, column 38>\n"+
-        "Detected a 'std::mutex', Name: 'mDataAccess' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 24, column 38>\n" + 
-        "Detected a 'std::mutex', Name: 'mDataAccess' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 37, column 16>\n" + 
-        "Detected a 'std::lock_guard<std::mutex>' Lockguard's Name: 'lock_guard' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 17, column 33>\n" + 
-        "Detected a 'std::lock_guard<std::mutex>' Lockguard's Name: 'lock_guard' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 24, column 33>\n" + 
-        "Detected std::string : 'mState' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 36, column 17>\n" + 
-        "Detected std::mutex : 'mDataAccess' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 37, column 16>\n" + 
-        "Detected MyClass : 'MyClass' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 5, column 7>\n\n" + 
-        "Detected MyClass : 'MyClass' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 5, column 7>\n\n" +
-        "Detected std::mutex : 'class std::mutex' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 17, column 26>\n" +
-        "Detected const std::basic_string<char> : 'mState' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 19, column 12>\n" +
-        "Detected std::mutex : 'class std::mutex' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 24, column 26>zn" + 
-        "Detected std::basic_string<char>: 'operator=' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 26, column 5>\n" + 
-        "Detected std::mutex : 'class std::mutex' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 37, column 10>\n" +
-        "Detected std::string () : 'getState()' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 15, column 13>\n" + 
-        "Detected void (const std::string &) : 'updateState(const std::string &)' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 22, column 6>\n" + 
-        "Detected void () : 'logState()' at <SourceLocation file '../cpp_tests/member_locked_in_some_methods/error_in_method_scope.cpp', line 29, column 6>"
-    )
-
-    output_str = mutex_observer.output + lock_guard_observer.output + declared_variable_observer.output + class_observer.output + record_observer.output + function_observer.output
-    assert(output_str) == correct_output
 
 
 # Unit test for member_locked_in_some_methods.py
