9d8ad3e0dc0af5096492c6fb8b70ec01f7648d5d
sprinac <123823081+sprinac@users.noreply.github.com>
2023-03-20 23:00:59 +0000

    update datamemberlock file

--- Checks/dataMemberLock.py
+++ Checks/dataMemberLock.py
@@ -11,7 +11,12 @@ class Check():
         alert_list: List[Alert] = list()
 
         #code
+
+        #print(f"{cursor.kind} at:\t({cursor.extent.start.line}, {cursor.extent.start.column})-({cursor.extent.end.line}, {cursor.extent.end.column})")
         
+        if (cursor.kind == clang.cindex.CursorKind.FIELD_DECL and cursor.access_specifier == clang.cindex.AccessSpecifier.PUBLIC):
+            #print(cursor.access_specifier)
+            print(f"{cursor.kind} at:\t({cursor.extent.start.line}, {cursor.extent.start.column})-({cursor.extent.end.line}, {cursor.extent.end.column})")
         return alert_list
         
     
