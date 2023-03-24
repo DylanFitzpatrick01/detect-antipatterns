9e06e89738a9c7d11a51f35fd30c12a69fa0ab2d
LeonByrne <leonbyrne52@gmail.com>
2023-03-24 15:23:47 +0000

    removed massive text files

--- cpp_tests/atomic.cpp
+++ cpp_tests/atomic.cpp
@@ -4,14 +4,13 @@ std::atomic_bool b;
 
 int main()
 {
-  bool a = b;
-  
-  bool c;
-  c = b;
+  bool a = b;     //Not atomic series of operations.
+  bool c;         //
+  c = b;          //
+  bool d = false; //
+  b = c;          //Error, b may have been changed since read above.
 
-  bool d = false;
-
-  b = c;
+  b = d;          //No error, d is independant of b.
 
   return 0;
 }
