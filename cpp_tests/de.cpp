4cc06e9864f65a9a7561259277030ce9de7c9905
LeonByrne <leonbyrne52@gmail.com>
2023-03-26 23:15:59 +0100

    Tidy up of ast.log files

--- cpp_tests/de.cpp
+++ cpp_tests/de.cpp
@@ -1,126 +1,43 @@
-#include <atomic>
-#include <chrono>
-#include <iostream>
-#include <thread>
-#include <memory>
+void a();
+void b();
+void c();
+void d();
+void e();
 
-
-class MyClass
+void a()
 {
-public:
-
-MyClass()
-    : mStopping(false)
-{
-    std::cout << "Creating MyClass instance, on thread " << std::this_thread::get_id() << "\n";
+    b();
 }
 
-void start()
+void b()
 {
-    std::cout << "Starting worker thread, on thread " << std::this_thread::get_id() << "\n";
-    mThread1 = std::make_unique<std::thread>([this](){
-
-        while(!mStopping)
-        {
-            std::cout << "Doing some work, on thread " << std::this_thread::get_id() << "\n";
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
-
-    });
+    c();
 }
 
-void stop()
+void c()
 {
-    if(mThread1)
+    d();
+}
+
+void d()
+{
+    if(true)
     {
-        mStopping = true;
-        std::cout << "Stopping worker thread, on thread " << std::this_thread::get_id() << "\n";
-
-        if(mThread1->joinable())
-        {
-            mThread1->join();
-        }
+        c();
+    } else
+    {
+        e();
     }
 }
 
-~MyClass()
+void e()
 {
-    std::cout << "MyClass desctructor, on thread " << std::this_thread::get_id() << "\n";
-    if(mThread1)
-    {
-        mStopping = true;
-        std::cout << "Stopping worker thread, on thread " << std::this_thread::get_id() << "\n";
-        if(mThread1->joinable())
-        {
-            mThread1->join();
-        }
-    }
-std::cout << "Creating MyClass instance, on thread " << std::this_thread::get_id() << "\n";
+
 }
-
-
-private:
-    std::unique_ptr<std::thread> mThread1;
-    std::unique_ptr<bool> mThread2;
-    std::thread mThread3;
-    std::thread* mThread4;
-    std::atomic<bool> mStopping;                    // This shared state is atomic, so it's safe to access on multiple threads.
-};
-
 
 int main()
 {
+    a();
 
-    // Scenario 1, correct usage
-    {
-        MyClass instance;
-
-        instance.start();
-
-        std::this_thread::sleep_for(std::chrono::seconds(5));
-
-        instance.stop();
-    }
-
-    // Scenario 2, incorrect usage... forget to call `stop()` before destruction
-    {
-        MyClass instance;
-
-        instance.start();
-
-        std::this_thread::sleep_for(std::chrono::seconds(5));
-
-    }   // In this case we end up calling the std::thread destructor without calling `join()`
-        // This is illegal and the program will exit, we wont get to the log line below.
-
-    std::cout << "Exiting program, on thread " << std::this_thread::get_id() << "\n";
+    return 0;
 }
-
-        // There's a design problem in `MyClass`. We are relying on the consumer to call `stop()`
-        // before the object is destroyed.
-
-        // It would be much better if we designed our class so the consumer doesnt need to remember
-        // to do this.
-
-        // The way to achieve this is to make sure  that the destructor itself handles the joining.
-        // Then we dont even need a `stop()`.
-
-        /** For example:
-
-            ~MyClass()
-            {
-                std::cout << "MyClass desctructor, on thread " << std::this_thread::get_id() << "\n";
-
-                if(mThread)
-                {
-                    mStopping = true;
-                    std::cout << "Stopping worker thread, on thread " << std::this_thread::get_id() << "\n";
-
-                    if(mThread->joinable())
-                    {
-                        mThread->join();
-                    }
-
-                }
-            }
-        **/
