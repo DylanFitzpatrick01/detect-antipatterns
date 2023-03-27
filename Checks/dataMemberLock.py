0b6a0d390ee89b9012ee38afe8a3b443a0da0143
sprinac <123823081+sprinac@users.noreply.github.com>
2023-03-27 21:04:05 +0100

    update datamember

--- Checks/dataMemberLock.py
+++ Checks/dataMemberLock.py
@@ -16,7 +16,16 @@ class Check():
         
         if (cursor.kind == clang.cindex.CursorKind.FIELD_DECL and cursor.access_specifier == clang.cindex.AccessSpecifier.PUBLIC):
             #print(cursor.access_specifier)
+            # count = 0
+            # contains = False
+            # for cursor1 in cursor.get_children():
+            #     count+= 1
+            #     if str(cursor1.displayname) != "class std::mutex" and str(cursor1.kind) == "CursorKind.TYPE_REF":
+            #         contains = True
+            #     if count > 2:
+            #         break
+            #     if contains and count == 2:
+            #         alert_list.apend(Alert(cursor.translation_unit, cursor.extent, "Are you sure you want to habe a public member called '" + cursor.displayname + "'?\n"
+            #                                "Consider making this mutex private"))
             print(f"{cursor.kind} at:\t({cursor.extent.start.line}, {cursor.extent.start.column})-({cursor.extent.end.line}, {cursor.extent.end.column})")
 
-            if():
-                print()
