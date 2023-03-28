28e2605707adbabca1de5e7092fd9e1ff36dcffb
sprinac <123823081+sprinac@users.noreply.github.com>
2023-03-28 15:38:13 +0100

    Update dataMemberLock.py

--- Checks/dataMemberLock.py
+++ Checks/dataMemberLock.py
@@ -16,25 +16,8 @@ class Check():
         #print(f"{cursor.kind} at:\t({cursor.extent.start.line}, {cursor.extent.start.column})-({cursor.extent.end.line}, {cursor.extent.end.column})")
         
         if (cursor.kind == clang.cindex.CursorKind.FIELD_DECL and cursor.access_specifier == clang.cindex.AccessSpecifier.PUBLIC):
-            
-
-
-        # if str(cursor.access_specifier) == "AccessSpecifier.PUBLIC":
-        #     #print(cursor.access_specifier)
-        #     count = 0
-        #     contains = False
-        #     for cursor1 in cursor.get_children():
-        #         count+= 1
-        #         if str(cursor1.displayname) == "std::mutex" and str(cursor1.kind) == "CursorKind.TYPE_REF":
-        #             contains = True
-        #         if count > 2:
-        #             break
-        #         if contains and count == 2:
-        #             # alert_list.apend(Alert(cursor.translation_unit, cursor.extent, "Are you sure you want to habe a public member called '" + cursor.displayname + "'?\n"
-        #             #                            "Consider making this mutex private"))
-        #             print("Are you sure you want to habe a public member called '" + cursor.displayname + "'?\n")
-                                     
-            print(f"{cursor.kind} at:\t({cursor.extent.start.line}, {cursor.extent.start.column})-({cursor.extent.end.line}, {cursor.extent.end.column})")
+                                      
+            # print(f"{cursor.kind} at:\t({cursor.extent.start.line}, {cursor.extent.start.column})-({cursor.extent.end.line}, {cursor.extent.end.column})")
             print("Are you sure you want a public member called '" +cursor.displayname+ "' consider making this data member private")
         
         if str(cursor.access_specifier) == "AccessSpecifier.PUBLIC":
@@ -44,11 +27,12 @@ class Check():
                     count += 1
                     if str(cursor1.displayname) != "class std::mutex" and str(cursor1.kind) == "CursorKind.TYPE_REF":
                         contains = True
+                        print(cursor1.displayname)
                     if count > 2:
                         break
                 if contains and count == 2:
                     alert_list.append(Alert(cursor.translation_unit, cursor.extent,
-                                            "Are you sure you want to have a public mutex called '" + cursor.displayname + "'?\n"
-                                            "Consider making this mutex private."))
+                                            "Are you sure you want to have a public data member called '" + cursor.displayname + "'?\n"
+                                            "Consider making this data member private."))
         
         return alert_list
