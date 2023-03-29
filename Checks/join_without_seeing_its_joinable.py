ed8cd60ddaa8025c38a04a04357a577b2c50f20c
John Wallace <jaywallace2002@gmail.com>
2023-03-29 19:31:49 +0100

    JW finsihed thread check code

--- Checks/join_without_seeing_its_joinable.py
+++ Checks/join_without_seeing_its_joinable.py
@@ -10,15 +10,17 @@ class Check():
     def analyse_cursor(self, cursor: clang.cindex.Cursor) -> List[Alert]:
         alert_list = list()
         joinDetected = 0
-        joiableDetected = 0
+        joinableDetected = 0
 
         for node in  cursor.translation_unit.cursor.walk_preorder():
-            if node.kind == clang.cindex.CursorKind.CALL_EXPR and node.spelling == "join":
-                               joinDetected =+ 1
-            elif node.kind == clang.cindex.CursorKind.CALL_EXPR and node.spelling == "joinable":
-                joiableDetected =+1
+                        if node.kind == clang.cindex.CursorKind.CALL_EXPR and node.spelling == "join" and node.type.spelling == "void": 
+                           joinDetected += 1
+                           print("Found std::thread::join() function call at line {0}".format(node.location.line))
+                        elif (node.kind == clang.cindex.CursorKind.CALL_EXPR and node.spelling == "joinable" and node.type.spelling == "bool"):
+                           joinableDetected += 1
+                           #print("Found std::thread::joinable() function call at line {0}".format(node.location.line))
 
 
-        if joinDetected > joiableDetected:
+        if joinDetected > joinableDetected:
              alert_list.append(Alert(cursor.translation_unit, cursor.extent,"Not all join functions check if thread is joinable",
                                      "\n Recommend adding a if( x.joinable) function"))
