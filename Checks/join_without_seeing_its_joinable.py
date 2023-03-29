64874643399e17d9269c0ec98e801b37715149d6
John Wallace <jaywallace2002@gmail.com>
2023-03-29 18:27:58 +0100

    JW updateing joinable function

--- Checks/join_without_seeing_its_joinable.py
+++ Checks/join_without_seeing_its_joinable.py
@@ -9,10 +9,16 @@ TODO Write Description
 class Check():
     def analyse_cursor(self, cursor: clang.cindex.Cursor) -> List[Alert]:
         alert_list = list()
+        joinDetected = 0
+        joiableDetected = 0
 
         for node in  cursor.translation_unit.cursor.walk_preorder():
             if node.kind == clang.cindex.CursorKind.CALL_EXPR and node.spelling == "join":
-                                alert_list.append(Alert(cursor.translation_unit, cursor.extent,+
-                                        "Recomend Add a joinable check "
-                                        "\n if (t.joinable())"
-                                        "\n t.join"))
+                               joinDetected =+ 1
+            elif node.kind == clang.cindex.CursorKind.CALL_EXPR and node.spelling == "joinable":
+                joiableDetected =+1
+
+
+        if joinDetected > joiableDetected:
+             alert_list.append(Alert(cursor.translation_unit, cursor.extent,"Not all join functions check if thread is joinable",
+                                     "\n Recommend adding a if( x.joinable) function"))
