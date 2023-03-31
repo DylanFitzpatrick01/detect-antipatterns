10d3d87a417587a8b5de7ea070f59909cf2c4658
LeonByrne <leonbyrne52@gmail.com>
2023-03-31 15:45:02 +0100

    Comments

--- cpp_tests/atomic.cpp
+++ cpp_tests/atomic.cpp
@@ -7,9 +7,9 @@ int main()
   bool a = b;     //Not atomic series of operations.
   bool c;         //
   c = b;          //
-  bool d = false; //
   b = c;          //Error, b may have been changed since read above.
 
+  bool d = false;
   b = d;          //No error, d is independant of b.
 
   return 0;
