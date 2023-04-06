7d31b6670ea88a629d658bc835f78c8174b10770
Leon Byrne <byrnel58@tcd.ie>
2023-04-06 22:35:29 +0100

    Reducing false positives

--- Checks/unsafeAtomics.py
+++ Checks/unsafeAtomics.py
@@ -11,6 +11,7 @@ class Check(FormalCheckInterface):
 		self.references = list()
 
 		self.atomicWrite = False
+		self.affectedSeen = 0
 
 		self.branchLevel = 0        # The level of branches we're in. 0 is none, >1
 																# is a nested branch
@@ -19,28 +20,38 @@ class Check(FormalCheckInterface):
 	# If entered method tell it next return
 
 	def analyse_cursor(self, cursor: clang.cindex.Cursor, alerts):
-		self.check_references()
+		if cursor.location.line == 19:
+			print("at the problem area")
 
 		if cursor.kind == clang.cindex.CursorKind.VAR_DECL or cursor.kind == clang.cindex.CursorKind.FIELD_DECL:
 			if "std::atomic" in cursor.type.spelling:
 				self.affected[cursor.referenced.get_usr()] = list()
 			else:
-				self.investagating = cursor
+				self.investigate_new(cursor)
+
 		elif cursor.kind == clang.cindex.CursorKind.CALL_EXPR and "operator" in cursor.spelling:
-			print(cursor.spelling)
+			# print(cursor.spelling)
 			if "atomic" in list(cursor.get_children())[0].type.spelling:
 				self.atomicWrite = True
-				self.investagating = list(cursor.get_children())[0]
+				self.investigate_new(list(cursor.get_children())[0])
 				self.skipNext = True
-		elif cursor.kind == clang.cindex.CursorKind.BINARY_OPERATOR and cursor.spelling == "=":
+
+		# This is some bullshit, so binary operators have an "operator type" cursor
+		# under them. Variables with operator in them will not be detected.
+		elif cursor.kind == clang.cindex.CursorKind.BINARY_OPERATOR and "operator" not in list(cursor.get_children())[0].spelling:
+			print("Binary ", list(cursor.get_children())[0].spelling)
 			self.atomicWrite = False
-			self.investagating = list(cursor.get_children())[0]
-			self.skipNext = True
+			self.investigate_new(list(cursor.get_children())[0])
+			self.skipNext = True 
+
 
 		elif cursor.kind == clang.cindex.CursorKind.DECL_REF_EXPR or cursor.kind == clang.cindex.CursorKind.MEMBER_REF_EXPR:
-			# Atomic to atomic writes arer okay, don't investigate
+			# Atomic to atomic writes are okay, don't investigate
+			# print(cursor.referenced.type.spelling)
 			if "std::atomic" in cursor.referenced.type.spelling and "std::atomic" not in self.investagating.referenced.type.spelling:
+				print(self.investagating.referenced.type.spelling)
 				self.affected[cursor.referenced.get_usr()].append(self.investagating.referenced)
+				self.affectedSeen += 1
 			elif not self.skipNext:
 				if self.atomicWrite:
 					for key in self.affected:
@@ -56,8 +67,11 @@ class Check(FormalCheckInterface):
 				else:
 					for key in self.affected:
 						for var in self.affected[key]:
-							if cursor.referenced.get_usr() == var.get_usr():
+							if cursor.referenced.get_usr() == var.get_usr() and "__atomic" not in self.investagating.type.spelling and self.investagating.referenced not in self.affected[key]:
+								print(cursor.referenced.type.spelling)
+								
 								self.affected[key].append(self.investagating.referenced)
+								self.affectedSeen += 1
 			elif self.skipNext:
 				self.skipNext = False
 
@@ -66,29 +80,42 @@ class Check(FormalCheckInterface):
 		#   for var in self.affected[key]:
 		#     print("  ", var.get_usr())
 
-	def check_references(self):
-		#TODO if one of the references is an atomic or atomic affected, add to the 
-		# 		dict. If none of them are remove it.
-		#			clear out references afterwards.
-		atomicRef = False
+	def investigate_new(self, cursor):
+		if self.affectedSeen == 0:
+			self.remove_affected(self.investagating)
 
-		for reference in self.references:
-			if key in self.affected:
-				if reference.referenced.get_usr() in self.affected[key]:
-					if self.investagating.referenced.get_usr() not in self.affected[key]:
-						self.affected[key].append(self.investagating.get_usr())
+		self.affectedSeen = 0
+		self.investagating = cursor
+
+	def remove_affected(self, cursor):
+		for key in self.affected:
+			for var in self.affected[key]:
+				if cursor.referenced.get_usr() == var.get_usr():
+					self.affected[key].remove(var)
+
+	# def check_references(self):
+	# 	#TODO if one of the references is an atomic or atomic affected, add to the 
+	# 	# 		dict. If none of them are remove it.
+	# 	#			clear out references afterwards.
+	# 	atomicRef = False
+
+	# 	for reference in self.references:
+	# 		if key in self.affected:
+	# 			if reference.referenced.get_usr() in self.affected[key]:
+	# 				if self.investagating.referenced.get_usr() not in self.affected[key]:
+	# 					self.affected[key].append(self.investagating.get_usr())
 			
-			elif "std::atomic" in reference.referenced.type.spelling: 
-				atomicRef = True
-				if self.investagating not in self.affected[reference.referenced.get_usr()]:
-					self.affected[reference.referenced.get_usr()].append(self.investagating.get_usr())
+	# 		elif "std::atomic" in reference.referenced.type.spelling: 
+	# 			atomicRef = True
+	# 			if self.investagating not in self.affected[reference.referenced.get_usr()]:
+	# 				self.affected[reference.referenced.get_usr()].append(self.investagating.get_usr())
 
-		if not atomicRef:
-			# remove investigating from affected
-			for key in self.affected:
-				self.affected[key].delete(self.investagating.referenced.get_usr())
+	# 	if not atomicRef:
+	# 		# remove investigating from affected
+	# 		for key in self.affected:
+	# 			self.affected[key].delete(self.investagating.referenced.get_usr())
 
-		self.references = list()
+	# 	self.references = list()
 
 	def __eq__(self, __o: object) -> bool:
 		if type(self) != type(__o):
--- cpp_tests/atomic.cpp
+++ cpp_tests/atomic.cpp
@@ -6,16 +6,19 @@ std::atomic_int y;
 
 int main()
 {
-  bool a = b;     //Not atomic series of operations.
-  bool c;         //
-  c = b;          //
-  b = c;          //Error, b may have been changed since read above.
+  // bool a = b;     //Not atomic series of operations.
+  // bool c;         //
+  // c = b;          //
+  // b = c;          //Error, b may have been changed since read above.
 
-  bool d = false;
-  b = d;          //No error, d is independant of b.
+  // bool d = false;
+  // b = d;          //No error, d is independant of b.
 
   int z = x * y;  // 
-  x += z;         //
+  x = z;          // error 
+  z = 5;          //
+  x += z;         // no error
+
 
   return 0;
 }
