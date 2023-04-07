24004aaf6af27aaae12114700108ba5e42a704d4
Liam Byrne <lbyrne15@gmail.com>
2023-04-07 11:21:50 +0100

    Fixed bugs

--- Checks/multiple_lock_order.py
+++ Checks/multiple_lock_order.py
@@ -1,6 +1,7 @@
 import clang.cindex
 from typing import List
 from alerts import Alert
+import re
 
 """
 A Check for the order of multiple locks. If found issue a warning.
@@ -9,28 +10,27 @@ A Check for the order of multiple locks. If found issue a warning.
 class Check():
     def analyse_cursor(self, cursor: clang.cindex.Cursor) -> List[Alert]:
         alert_list = list()
-        heldLocks = list()
-        usedLocks = list()
-        mutex_names = []
-        for match in cursor.get_children():
-            if (str(match.displayname) == "std::lock_guard" and str(match.type.spelling) == "class std::mutex"):
-                for c in match.get_children():
-                    if (str(c.kind) == "CXX_MEMBER_CALL_EXPR" and str(c.spelling) == "lock"):
-                        for a in c.get_children():
-                            if (str(a.kind) == "DECL_REF_EXPR" and str(a.type.spelling) == "class std::mutex"):
-                                mutex_names.append(a.spelling)
-        multi_order = list(mutex_names)
-        highestIndex = 0
-        for mutex in multi_order:
-            if mutex in heldLocks:
-                if mutex in usedLocks:
-                    usedLocks.clear()
+        if cursor.kind == clang.cindex.CursorKind.CALL_EXPR:
+            function_name = cursor.spelling
+            if function_name == "lock":
+                mutex_names = []
+                for child in cursor.get_children():
+                    if child.kind == clang.cindex.CursorKind.DECL_REF_EXPR:
+                        mutex_names.append(child.spelling)
+                multi_order = list(mutex_names)
+                heldLocks = list()
+                usedLocks = list()
                 highestIndex = 0
-                usedLocks.append(mutex)
-                if (heldLocks.index(mutex) >= highestIndex):
-                    highestIndex = heldLocks.index(mutex)
-                else:
-                    alert_list.append(Alert(cursor.translation_unit, cursor.extent, "Error!: mutex " + str(mutex) + " is in the incorrect order!\n"))
-            else:
-                heldLocks.append(mutex)
+                for mutex in multi_order:
+                    if mutex in heldLocks:
+                        if mutex in usedLocks:
+                            usedLocks.clear()
+                        highestIndex = 0
+                        usedLocks.append(mutex)
+                        if (heldLocks.index(mutex) >= highestIndex):
+                            highestIndex = heldLocks.index(mutex)
+                        else:
+                            alert_list.append(Alert(cursor.translation_unit, cursor.extent, "Error!: mutex " + str(mutex) + " is in the incorrect order!\n"))
+                    else:
+                        heldLocks.append(mutex)
         return alert_list
